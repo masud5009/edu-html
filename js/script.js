98d06c72f3bcfb04c9d6bcb187a7b0fc7ba65a9a
Masud Rana <ranaahmed269205@gmail.com>
2024-10-30 22:24:23 +0600

    navbar for mobail menu

--- js/script.js
+++ js/script.js
@@ -1,29 +1,36 @@
+"use strict";
+$('.navbar-toggler').on('click', function () {
+    const offcanvasMenuItems = $('#navbarMenu .nav-item').clone();
+    $('#offcanvasMenuItems').empty().append(offcanvasMenuItems);
+});
+/*======= Initialize AOS animations =======*/
 AOS.init();
-//template scroll
-$(document).ready(function () {
-    $('.template-card').hover(
-      function () {
-        // Calculate the height of the image to scroll completely out of view
-        const imgHeight = $(this).find('.template-img').outerHeight();
-        $(this).find('.template-img').stop().animate({ top: `-${imgHeight}px` }, 5000); // Scroll up completely
-      },
-      function () {
-        $(this).find('.template-img').stop().animate({ top: '0' }, 5000); // Scroll back to original position
-      }
-    );
-  });
 
-  //testimonial slider 
-  $(document).ready(function () {
+/*========== template scroll =======*/
+$(function () {
+    $('.template-card').hover(
+        function () {
+            // Calculate the height of the image to scroll completely out of view
+            const imgHeight = $(this).find('.template-img').outerHeight();
+            $(this).find('.template-img').stop().animate({ top: `-${imgHeight}px` }, 5000); // Scroll up completely
+        },
+        function () {
+            $(this).find('.template-img').stop().animate({ top: '0' }, 5000); // Scroll back to original position
+        }
+    );
+
+    /*========== testimonial slider =======*/
     $('.testimonial-slider').slick({
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 2,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      vertical: true,
-      adaptiveHeight: true,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        vertical: true,
+        adaptiveHeight: true,
     });
-  });
+
+
+});
