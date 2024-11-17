395dbe620526a0bb38f6a7cb75b6eb904be5aa50
Masud Rana <ranaahmed269205@gmail.com>
2024-11-18 00:20:01 +0600

    user profile section desing

--- js/script.js
+++ js/script.js
@@ -62,8 +62,24 @@ $(function () {
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
-        adaptiveHeight: true,
-    });
+        responsive: [
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+      
 });
 
 $(document).ready(function(){
