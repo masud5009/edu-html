ea092e7fd1fe91021d7617a54b79346223482fa2
Masud Rana <ranaahmed269205@gmail.com>
2024-11-21 00:53:15 +0600

    change color family

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
 "use strict";
 /*======= navbar js =======*/
 $('.navbar-toggler').on('click', function () {
-    const offcanvasMenuItems = $('#navbarMenu .nav-item').clone();
-    $('#offcanvasMenuItems').empty().append(offcanvasMenuItems);
+  const offcanvasMenuItems = $('#navbarMenu .nav-item').clone();
+  $('#offcanvasMenuItems').empty().append(offcanvasMenuItems);
 });
 document.addEventListener("scroll", function () {
-    const navbar = document.querySelector(".navbar");
-    
-    // Check if the scroll position is greater than 50px (or any value you prefer)
-    if (window.scrollY > 50) {
-      navbar.classList.add("scrolled");
-    } else {
-      navbar.classList.remove("scrolled");
-    }
-  });
-  
+  const navbar = document.querySelector(".navbar");
+
+  // Check if the scroll position is greater than 50px (or any value you prefer)
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
 //  trigger resize base on window size
 $(window).resize(function () {
-    if ($(window).width() <= 992) {
-        $('#offcanvasMenuItems').empty().append(offcanvasMenuItems);
-    }else{
-        $('.btn-close').trigger('click');
-    }
+  if ($(window).width() <= 992) {
+    $('#offcanvasMenuItems').empty().append(offcanvasMenuItems);
+  } else {
+    $('.btn-close').trigger('click');
+  }
 }).trigger('resize');
 
 
@@ -30,59 +30,66 @@ AOS.init();
 
 /*========== template scroll =======*/
 $(function () {
-    $('.template-card').hover(
-        function () {
-            // Calculate the height of the image to scroll completely out of view
-            const imgHeight = $(this).find('.template-img').outerHeight();
-            $(this).find('.template-img').stop().animate({ top: `-${imgHeight}px` }, 5000); // Scroll up completely
-        },
-        function () {
-            $(this).find('.template-img').stop().animate({ top: '0' }, 5000); // Scroll back to original position
-        }
-    );
+  $('.template-card').hover(
+    function () {
+      let templateImg = $(this).find('.template-img');
 
-    /*========== testimonial slider =======*/
-    $('.testimonial-slider').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        vertical: true,
-        adaptiveHeight: true,
-    });
-    /*========== user profile slider =======*/
-    $('.user-slider').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        responsive: [
-          {
-            breakpoint: 1200,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 768,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
-      });
-      
+      if (templateImg.length) {
+        let imgHeight = templateImg.outerHeight();
+        templateImg.stop(true, true).animate({ top: `-${imgHeight}px` }, 5000);
+      }
+    },
+    function () {
+      let templateImg = $(this).find('.template-img');
+
+      if (templateImg.length) {
+        templateImg.stop().animate({ top: '0' }, 5000);
+      }
+    }
+  );
+
+  /*========== testimonial slider =======*/
+  $('.testimonial-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    vertical: true,
+    adaptiveHeight: true,
+  });
+  /*========== user profile slider =======*/
+  $('.user-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
 });
 
-$(document).ready(function(){
-    let breadcrumbImage = $('.breadcrumb-section').data('image');
-    $('.breadcrumb-section').css('background-image', 'url(' + breadcrumbImage + ')');
+$(document).ready(function () {
+  let breadcrumbImage = $('.breadcrumb-section').data('image');
+  $('.breadcrumb-section').css('background-image', 'url(' + breadcrumbImage + ')');
 });
